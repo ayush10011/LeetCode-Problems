3cd689d69bd5f7af51b5559a4d0422824587e5ac
AYUSH UMREDKAR <142901524+ayush10011@users.noreply.github.com>
2026-06-22 21:46:49 +0530

    Time: 16 ms (39.5%), Space: 12.4 MB (88.97%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,19 @@
+class Solution:
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+        curr_row = 0
+        direction = -1  # will flip at boundaries
+
+        for char in s:
+            rows[curr_row] += char
+
+            # Change direction at top/bottom
+            if curr_row == 0 or curr_row == numRows - 1:
+                direction *= -1
+
+            curr_row += direction
+
+        return "".join(rows)
